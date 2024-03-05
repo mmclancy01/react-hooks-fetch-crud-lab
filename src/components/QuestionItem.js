7697bda3792b4cb85009e8f153c22ac786e9fe91
Matthew Clancy <mmclancy01@yahoo.com>
2024-03-04 21:06:19 -0500

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, HandleDelete }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -17,7 +19,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={()=> HandleDelete(id)}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,26 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions, setQuestions}) {
+  
+  function HandleDelete(id) {
+    let newQuestionList = questions.filter((item)=> item.id !== id)
+    fetch("http://localhost:4000/questions/" + id , {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => setQuestions(newQuestionList));
+  
+  }
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      
+      <ul>{questions.map((question, index)=> ( 
+        <QuestionItem question={question} key={index} HandleDelete= {HandleDelete} />
+      ))}
+      </ul>
     </section>
   );
 }
